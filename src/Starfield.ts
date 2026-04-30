027fb68228550d5c36059714ec6ea88486465b47
CRISvsGAME <cris@crisvsgame.com>
2026-04-30 20:00:00 +0000

    Starfield 0.0.8
    Add Starfield Constructor

--- src/Starfield.ts
+++ src/Starfield.ts
@@ -1,1 +1,13 @@
-export class Starfield {}
+export class Starfield {
+    private cvs: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+
+    constructor(cvs: HTMLCanvasElement) {
+        this.cvs = cvs;
+        this.ctx = cvs.getContext("2d")!;
+
+        if (!this.ctx) {
+            throw new Error("Failed to get 2D context");
+        }
+    }
+}
