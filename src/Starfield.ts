4f7e12072a66132053a8945cca5e711469d1a403
CRISvsGAME <cris@crisvsgame.com>
2026-05-01 20:00:00 +0000

    Starfield 0.0.10
    Refactor Starfield Class

--- src/Starfield.ts
+++ src/Starfield.ts
@@ -4,21 +4,19 @@ export class Starfield {
     private dpr: number = window.devicePixelRatio || 1;
 
     constructor(cvs: HTMLCanvasElement) {
-        this.cvs = cvs;
-        this.ctx = cvs.getContext("2d")!;
+        const ctx = cvs.getContext("2d");
 
-        if (!this.ctx) {
-            throw new Error("Failed to get 2D context");
-        }
+        if (!ctx) throw new Error("Failed to get 2D context");
+
+        this.cvs = cvs;
+        this.ctx = ctx;
 
         this.resize();
 
-        window.addEventListener("resize", () => {
-            this.resize();
-        });
+        window.addEventListener("resize", this.resize);
     }
 
-    private resize() {
+    private resize = (): void => {
         const dpr = window.devicePixelRatio || 1;
         const bcr = this.cvs.getBoundingClientRect();
 
@@ -26,5 +24,5 @@ export class Starfield {
         this.cvs.width = bcr.width * dpr;
         this.cvs.height = bcr.height * dpr;
         this.ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
-    }
+    };
 }
