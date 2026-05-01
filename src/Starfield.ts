5edc5e6e06bc1983a9e4ee87db0dcde8a5245ba4
CRISvsGAME <cris@crisvsgame.com>
2026-05-01 20:00:00 +0000

    Starfield 0.0.9
    Add Starfield Resize

--- src/Starfield.ts
+++ src/Starfield.ts
@@ -1,6 +1,7 @@
 export class Starfield {
     private cvs: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
+    private dpr: number = window.devicePixelRatio || 1;
 
     constructor(cvs: HTMLCanvasElement) {
         this.cvs = cvs;
@@ -9,5 +10,21 @@ export class Starfield {
         if (!this.ctx) {
             throw new Error("Failed to get 2D context");
         }
+
+        this.resize();
+
+        window.addEventListener("resize", () => {
+            this.resize();
+        });
+    }
+
+    private resize() {
+        const dpr = window.devicePixelRatio || 1;
+        const bcr = this.cvs.getBoundingClientRect();
+
+        this.dpr = dpr;
+        this.cvs.width = bcr.width * dpr;
+        this.cvs.height = bcr.height * dpr;
+        this.ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
     }
 }
