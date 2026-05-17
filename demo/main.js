ef12e919e88d92b7bd6b55308c6607cf07a90d3f
CRISvsGAME <cris@crisvsgame.com>
2026-05-17 20:00:00 +0000

    Starfield 1.0.1
    Add Demo

---
+++ demo/main.js
@@ -0,0 +1,10 @@
+import { Starfield } from "https://cdn.jsdelivr.net/npm/@crisvsgame/starfield@1.0.0/dist/index.js";
+
+const cvs = document.getElementById("starfield");
+
+if (!(cvs instanceof HTMLCanvasElement)) {
+    throw new Error("Canvas element not found");
+}
+
+const starfield = new Starfield(cvs);
+starfield.start();
