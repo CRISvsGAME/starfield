68dde810d4f4cceaaf30efe1525b9ba0f513c213
CRISvsGAME <cris@crisvsgame.com>
2026-05-23 20:00:00 +0000

    Starfield 1.0.12
    Add ContrastRGB Helper

--- docs/main.js
+++ docs/main.js
@@ -6,15 +6,21 @@ if (!(cvs instanceof HTMLCanvasElement)) {
     throw new Error("Canvas element not found");
 }
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-const randomRgb = () => ({
-    r: randomInt(0, 255),
-    g: randomInt(0, 255),
-    b: randomInt(0, 255),
-});
+const randomRgb = () => {
+    return {
+        r: randomInt(0, 255),
+        g: randomInt(0, 255),
+        b: randomInt(0, 255),
+    };
+};
 
-const rgbToCss = ({ r, g, b }) => `rgb(${r}, ${g}, ${b})`;
+const rgbToCss = ({ r, g, b }) => {
+    return `rgb(${r}, ${g}, ${b})`;
+};
 
 const rgbToLinear = (channel) => {
     const normalized = channel / 255;
@@ -26,7 +32,13 @@ const relativeLuminance = ({ r, g, b }) => {
     return 0.2126 * rgbToLinear(r) + 0.7152 * rgbToLinear(g) + 0.0722 * rgbToLinear(b);
 };
 
-const isDarkColor = (luminance) => luminance < 0.5;
+const isDarkColor = (luminance) => {
+    return luminance < 0.5;
+};
+
+const constrastRgb = (isDark) => {
+    return isDark ? { r: 255, g: 255, b: 255 } : { r: 0, g: 0, b: 0 };
+};
 
 const backgroundRgb = randomRgb();
 const backgroundColor = rgbToCss(backgroundRgb);
