6530ade58fcf38b05d6ed0afcd71cf27f9bc323f
CRISvsGAME <cris@crisvsgame.com>
2026-05-24 20:00:00 +0000

    Starfield 1.0.14
    Add RandomFloat Method

--- docs/main.js
+++ docs/main.js
@@ -10,6 +10,10 @@ const randomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const randomFloat = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
 const randomRgb = () => {
     return {
         r: randomInt(0, 255),
